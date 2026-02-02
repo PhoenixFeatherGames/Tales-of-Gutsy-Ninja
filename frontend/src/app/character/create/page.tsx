9f49db1caa1bcf9a18b3cb56429c3e23f21848e6
PhoenixFeatherGames <thewhitephoenix@yahoo.com>
2026-02-01 22:40:21 -0800

    Fix: remove duplicate code and extra brace from page.tsx

--- frontend/src/app/character/create/page.tsx
+++ frontend/src/app/character/create/page.tsx
@@ -9,15 +9,3 @@ export default function CharacterCreatePage() {
     </main>
   );
 }
-"use client";
-
-
-
-export default function CharacterCreatePage() {
-  return (
-    <main className="p-6 max-w-3xl mx-auto">
-      <CharacterCreationForm />
-    </main>
-  );
-}
-}
